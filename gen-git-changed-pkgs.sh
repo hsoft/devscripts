472a0d9296bdce3da4ab1c293ce34003824a23f2
Virgil Dupras <hsoft@hardcoded.net>
2018-06-10 10:03:25 -0400

    gen-git-changed-pkgs.sh: weed out bad atoms
    Use `qatom` to validate atom names parsed from git comments

--- gen-git-changed-pkgs.sh
+++ gen-git-changed-pkgs.sh
@@ -6,4 +6,8 @@
 # Example
 # gen-git-changed-pkgs.sh HEAD~100
 
-git log --format="%s" $1..HEAD | grep -P -o "^[^\s/]+/[^\s:]+" | sort | uniq
+git log --format="%s" $1..HEAD \
+    | grep -P -o "^.+(?=:)" \
+    | sort | uniq \
+    | xargs -L1 -- qatom -F "%{CATEGORY}/%{PN}" \
+    | grep -v "(null)"
